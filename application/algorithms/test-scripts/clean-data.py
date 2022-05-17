f3d8b4718bd972be62496bfa5f13a3405a45e899
Oscar Vian Valles <oscarvianvalles@gmail.com>
2022-05-17 15:36:30 +0800

    data: clean key error

--- application/algorithms/test-scripts/clean-data.py
+++ application/algorithms/test-scripts/clean-data.py
@@ -3,12 +3,12 @@ from os import walk
 
 
 def main():
-    (_, _, filenames) = next(walk("../dataset/test"))
+    (_, _, filenames) = next(walk("../dataset/training"))
     files = [file for file in filenames if not "mp4" in file]
 
     for file in files:
-        with open(f"../dataset/test/{file}", "r+") as f:
-            content = f.read().replace(" ", "")
+        with open(f"../dataset/training/{file}", "r+") as f:
+            content = f.read().replace(" ", "").replace("//", "/")
             f.seek(0)
             f.write(content)
             f.truncate()
